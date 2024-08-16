9783d2204c94e9120d32b0edce6cecb52787d274
hari-shanke-reddy <harishankerreddy70@gmail.com>
2024-08-16 11:47:00 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,180 @@
+const logotext = "HARI";
+const meta = {
+  title: "Hari Shanker",
+  description: "I’m Hari Shanker _ Full stack devloper",
+};
+
+const introdata = {
+  title: "I’m Hari Shanker",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    " I'm  a passionate MEARN stack developer with a knack for creating dynamic and responsive web applications. With a strong foundation in MongoDB, Express.js, React.js, and Node.js, I bring ideas to life through clean code and modern design principles.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "My journey into web development started out of pure curiosity during my college days, and it quickly evolved into a fulfilling career. I enjoy the entire process of creating something from scratch—whether it’s designing the architecture of an application or fine-tuning the user interface for a seamless experience. For me, coding isn’t just a job; it’s a craft that I’m continually refining.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Software Developer",
+    where: "Vidyardi Institutions Pvt Ltd [Hyderabad]",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React.js",
+    value: 70,
+  },
+  {
+    name: "Node.js",
+    value: 75,
+  },
+  {
+    name: "Express.js",
+    value: 70,
+  },
+  {
+    name: "MongoDB",
+    value: 75,
+  },
+  {
+    name: "Redux",
+    value: 75,
+  },
+];
+
+const services = [
+  {
+    title: "Full-Stack Web Development",
+    description:
+      "From front-end design to back-end architecture, I provide end-to-end development services. I specialize in building responsive, scalable, and secure web applications using the MEARN stack—MongoDB, Express.js, React.js, and Node.js.",
+  },
+  {
+    title: "Database Design & Management",
+    description:
+      "With expertise in MongoDB, I provide database design and management services that ensure your data is structured, accessible, and secure. I optimize database performance and implement best practices for scalability.",
+  },
+  {
+    title: "Custom API Development",
+    description:
+      "Need a custom API to connect different parts of your application or to integrate third-party services? I design and implement RESTful APIs tailored to your specific requirements, ensuring seamless data flow and robust performance.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "harishankerreddy70@gmail.com",
+  YOUR_FONE: "+91 7660940729",
+  description:
+    " Whether you have a project in mind, need technical advice, or just want to connect, I’d love to hear from you! Feel free to reach out to me using the form below or through any of my social media profiles. I’m always open to discussing new ideas, collaborating on exciting projects, or answering any questions you might have.",
+  YOUR_SERVICE_ID: "service_9vol2tu",
+  YOUR_TEMPLATE_ID: "template_kvl02vr",
+  YOUR_USER_ID: "3yp9xW8fHqdHLN5Bv",
+};
+
+const socialprofils = {
+  github: "https://github.com/hari-shanke-reddy",
+  facebook: "https://facebook.com",
+  linkedin: "www.linkedin.com/in/harishanker-reddy-b271851b5",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
